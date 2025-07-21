0369d6362b7ba37d3d5664f1b3d1d0b97d87534b
Hamza07-dotcom <hthamzat355@gmail.com>
2025-07-21 17:46:17 +0500

    changes
    changes

--- weather-app/weather.js
+++ weather-app/weather.js
@@ -13,8 +13,8 @@ const weather_body = document.querySelector('.weather-body');
 
 async function checkWeather(city){
     const api_key = "241f7f9124f81a74d98d40f8017d86b3";
-    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-    const url=`http://goweather.xyz/weather/Okara`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    // const url=`http://goweather.xyz/weather/Okara`;
     const weather_data = await fetch(`${url}`).then(response => response.json());
 
 
