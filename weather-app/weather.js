ae8955b2798f3e479853fa8e7cbac8966b3b54fb
Hamza07-dotcom <hthamzat355@gmail.com>
2025-07-21 17:53:07 +0500

    changed

--- weather-app/weather.js
+++ weather-app/weather.js
@@ -5,6 +5,8 @@ const temperature = document.querySelector('.temperature');
 const description = document.querySelector('.description');
 const humidity = document.getElementById('humidity');
 const wind_speed = document.getElementById('wind-speed');
+const button=document.getElementById('btn');
+
 
 const location_not_found = document.querySelector('.location-not-found');
 
